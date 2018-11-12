7ad681d67b9317df3942a3f7eab0652ded45a481
David Paller <dpaller94@gmail.com>
2018-11-12 18:56:47 -0500

    Added DI for Data layer

--- Refuge.Core/Startup.cs
+++ Refuge.Core/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Refuge.Application.Interfaces;
+using Refuge.Data;
 
 namespace Refuge.Core
 {
@@ -26,6 +28,9 @@ namespace Refuge.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register data DI
+            services.AddDataLayer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Refuge.Data/ServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Refuge.Data.Context;
+using Refuge.Data.Interfaces;
+using Refuge.Data.Interfaces.Context;
+using Refuge.Data.Interfaces.Repositories;
+using Refuge.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refuge.Data
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddDataLayer(this IServiceCollection services)
+        {
+            services.AddTransient<IClassRepository, ClassRepository>();
+            services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<IClassContext, ClassContext>();
+            services.AddSingleton<IConfigValues, ConfigValues>();
+
+            return services;
+        }
+    }
+}
